e801bc12f51b512ba2bffc3f9eb6fc93e6fa5b96
Konrad Wojas <github@m.wojas.nl>
2020-04-21 22:15:01 +0800

    Refactor set-position tool

--- plugin/set-position.cpp
+++ plugin/set-position.cpp
@@ -1,16 +1,9 @@
-#ifdef _WIN32
-#include <Windows.h>
-#else
-#include <unistd.h>
-#endif
-
 #include <cstdio>
 #include <ctime>
-
 #include <string>
 
-#include <nng/nng.h>
 #include <fmt/core.h>
+#include <nng/nng.h>
 #include <nng/protocol/reqrep0/req.h>
 
 #include "xplane.pb.h"
@@ -20,8 +13,33 @@ void nng_fatal(const char *func, int rv) {
   exit(1);
 }
 
-nng_socket sock;
-int rv;
+
+std::unique_ptr<xplane::Response>
+do_rpc(nng_socket sock, const std::unique_ptr<xplane::Request> & req) {
+  // Always return a Response, also for local and connection errors
+  auto rep = std::make_unique<xplane::Response>();
+  auto pb = req->SerializeAsString();
+  int rv;
+
+  fmt::print("Sending request\n");
+  if ((rv = nng_send(sock, (void *) pb.c_str(), pb.length(), 0)) != 0) {
+    rep->set_error(nng_strerror(rv));
+    return rep;
+  }
+  fmt::print("Request sent\n");
+
+  fmt::print("Reading response\n");
+  size_t sz;
+  char * buf = nullptr;
+  if ((rv = nng_recv(sock, &buf, &sz, NNG_FLAG_ALLOC)) != 0) {
+    rep->set_error(nng_strerror(rv));
+    return rep;
+  }
+  fmt::print("Read response\n");
+  rep->ParseFromArray(buf, sz);
+  nng_free(buf, sz);
+  return rep;
+}
 
 int main(int argc, char* argv[]) {
   if (argc < 3) {
@@ -32,42 +50,36 @@ int main(int argc, char* argv[]) {
   double lon = std::stod(argv[2]);
   double elev = std::stod(argv[3]);
 
+  // Create socket and connect
+  nng_socket sock;
+  int rv;
   if ((rv = nng_req0_open(&sock)) != 0) {
     nng_fatal("nng_req0_open", rv);
+  }
+  if ((rv = nng_setopt_ms(sock, NNG_OPT_SENDTIMEO, 1000)) != 0) {
+    nng_fatal("nng_setopt_ms NNG_OPT_SENDTIMEO", rv);
+  }
+  if ((rv = nng_setopt_ms(sock, NNG_OPT_RECVTIMEO, 1000)) != 0) {
+    nng_fatal("nng_setopt_ms NNG_OPT_RECVTIMEO", rv);
   }
   constexpr auto& url = "tcp://127.0.0.1:27471";
   if ((rv = nng_dial(sock, url, nullptr, 0)) != 0) {
     nng_fatal("nng_dial", rv);
   }
 
+  // Build request
   auto req = std::make_unique<xplane::Request>();
   req->set_command(xplane::Request_Command_SetPosition);
   auto pos = req->mutable_position();
   pos->set_lat(lat);
   pos->set_lon(lon);
   pos->set_elev(elev);
-  auto pb = req->SerializeAsString();
   req->PrintDebugString();
 
-  fmt::print("Sending request\n");
-  if ((rv = nng_send(sock, (void *) pb.c_str(), pb.length(), 0)) != 0) {
-    nng_fatal("nng_send", rv);
-  }
-  fmt::print("Request sent\n");
-
-  fmt::print("Reading response\n");
-  size_t sz;
-  char * buf = nullptr;
-  if ((rv = nng_recv(sock, &buf, &sz, NNG_FLAG_ALLOC)) != 0) {
-    nng_fatal("nng_recv", rv);
-  }
-  fmt::print("Read response\n");
-
-  auto rep = std::make_unique<xplane::Response>();
-  rep->ParseFromArray(buf, sz);
+  // Do call
+  auto rep = do_rpc(sock, req);
   rep->PrintDebugString();
 
-  nng_free(buf, sz);
   nng_close(sock);
   return 0;
 }
